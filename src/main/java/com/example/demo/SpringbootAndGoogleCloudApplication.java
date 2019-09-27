2d03253ac1411361a8a561fe5d2bf3d46087ba8d
Francisco Villegas <francisco.villegas@peoplemanager.cl>
2019-09-27 15:29:00 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/SpringbootAndGoogleCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAndGoogleCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAndGoogleCloudApplication.class, args);
+	}
+
+}
